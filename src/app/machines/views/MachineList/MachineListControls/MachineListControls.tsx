86a2278487ded2dbe9b4ac040b03f526a86b50f0
Peter Makowski <me+git@petermakowski.io>
2023-03-31 12:48:08 +0200

    add container query styles

--- src/app/machines/views/MachineList/MachineListControls/MachineListControls.tsx
+++ src/app/machines/views/MachineList/MachineListControls/MachineListControls.tsx
@@ -94,106 +94,108 @@ const MachineListControls = ({
           {resourcePoolsCount} {pluralize("pool", resourcePoolsCount)}
         </Link>
       </h1>
-      {!hasSelection ? (
-        <>
-          <div>
-            <MachinesFilterAccordion
-              searchText={searchText}
-              setSearchText={(searchText) => {
-                setFilter(searchText);
-              }}
-            />
-          </div>
-          <div className="u-flex--grow">
-            <DebounceSearchBox
-              onDebounced={(debouncedText) => setFilter(debouncedText)}
-              searchText={searchText}
-              setSearchText={setSearchText}
-            />
-          </div>
-          <div>
-            <div className="u-flex--align-baseline">
-              <div className="u-flex--grow">
-                <GroupSelect
-                  grouping={grouping}
-                  setGrouping={setGrouping}
-                  setHiddenGroups={setHiddenGroups}
-                />
+      <div className="machine-list-controls-inputs">
+        {!hasSelection ? (
+          <>
+            <div className="u-flex">
+              <MachinesFilterAccordion
+                searchText={searchText}
+                setSearchText={(searchText) => {
+                  setFilter(searchText);
+                }}
+              />
+            </div>
+            <div className="u-flex--grow">
+              <DebounceSearchBox
+                onDebounced={(debouncedText) => setFilter(debouncedText)}
+                searchText={searchText}
+                setSearchText={setSearchText}
+              />
+            </div>
+            <div className="u-hide--small u-hide--medium">
+              <div className="u-flex--align-baseline">
+                <div className="u-flex--grow">
+                  <GroupSelect
+                    grouping={grouping}
+                    setGrouping={setGrouping}
+                    setHiddenGroups={setHiddenGroups}
+                  />
+                </div>
               </div>
             </div>
-          </div>
-        </>
-      ) : (
-        <>
-          <div>
-            <NodeActionMenuGroup
-              alwaysShowLifecycle
-              excludeActions={[NodeActions.IMPORT_IMAGES]}
-              getTitle={getTitle}
-              hasSelection={hasSelection}
-              nodeDisplay="machine"
-              onActionClick={(action) => {
-                if (action === NodeActions.TAG && !tagsSeen) {
-                  setTagsSeen(true);
+          </>
+        ) : (
+          <>
+            <div>
+              <NodeActionMenuGroup
+                alwaysShowLifecycle
+                excludeActions={[NodeActions.IMPORT_IMAGES]}
+                getTitle={getTitle}
+                hasSelection={hasSelection}
+                nodeDisplay="machine"
+                onActionClick={(action) => {
+                  if (action === NodeActions.TAG && !tagsSeen) {
+                    setTagsSeen(true);
+                  }
+                  const view = Object.values(MachineHeaderViews).find(
+                    ([, actionName]) => actionName === action
+                  );
+                  if (view) {
+                    setSidePanelContent({ view });
+                  }
+                  sendAnalytics(
+                    "Machine list action form",
+                    getNodeActionTitle(action),
+                    "Open"
+                  );
+                }}
+              />
+              <NodeActionMenu
+                alwaysShowLifecycle
+                excludeActions={[NodeActions.IMPORT_IMAGES]}
+                getTitle={getTitle}
+                hasSelection={hasSelection}
+                nodeDisplay="machine"
+                onActionClick={(action) => {
+                  if (action === NodeActions.TAG && !tagsSeen) {
+                    setTagsSeen(true);
+                  }
+                  const view = Object.values(MachineHeaderViews).find(
+                    ([, actionName]) => actionName === action
+                  );
+                  if (view) {
+                    setSidePanelContent({ view });
+                  }
+                  sendAnalytics(
+                    "Machine list action form",
+                    getNodeActionTitle(action),
+                    "Open"
+                  );
+                }}
+                toggleAppearance=""
+                toggleClassName="p-action-menu filter-accordion__toggle u-no-margin--bottom"
+                toggleLabel="Menu"
+              />
+            </div>
+            <div className="u-flex--center">
+              <Button
+                appearance="link"
+                onClick={() =>
+                  dispatch(machineActions.setSelectedMachines(null))
                 }
-                const view = Object.values(MachineHeaderViews).find(
-                  ([, actionName]) => actionName === action
-                );
-                if (view) {
-                  setSidePanelContent({ view });
-                }
-                sendAnalytics(
-                  "Machine list action form",
-                  getNodeActionTitle(action),
-                  "Open"
-                );
-              }}
-            />
-            <NodeActionMenu
-              alwaysShowLifecycle
-              excludeActions={[NodeActions.IMPORT_IMAGES]}
-              getTitle={getTitle}
-              hasSelection={hasSelection}
-              nodeDisplay="machine"
-              onActionClick={(action) => {
-                if (action === NodeActions.TAG && !tagsSeen) {
-                  setTagsSeen(true);
-                }
-                const view = Object.values(MachineHeaderViews).find(
-                  ([, actionName]) => actionName === action
-                );
-                if (view) {
-                  setSidePanelContent({ view });
-                }
-                sendAnalytics(
-                  "Machine list action form",
-                  getNodeActionTitle(action),
-                  "Open"
-                );
-              }}
-              toggleAppearance=""
-              toggleClassName="p-action-menu filter-accordion__toggle u-no-margin--bottom"
-              toggleLabel="Menu"
-            />
-          </div>
-          <div className="u-flex--center">
-            <Button
-              appearance="link"
-              onClick={() => dispatch(machineActions.setSelectedMachines(null))}
-            >
-              Clear selection <Icon name="close-link" />
-            </Button>
-          </div>
-        </>
-      )}
-
-      <AddHardwareMenu
-        disabled={hasSelection}
-        key="add-hardware"
-        setSidePanelContent={setSidePanelContent}
-      />
-
-      <div>
+              >
+                Clear selection <Icon name="close-link" />
+              </Button>
+            </div>
+          </>
+        )}
+        <div className="u-hide--small u-hide--medium">
+          <AddHardwareMenu
+            disabled={hasSelection}
+            key="add-hardware"
+            setSidePanelContent={setSidePanelContent}
+          />
+        </div>
         <HiddenColumnsSelect
           hiddenColumns={hiddenColumns}
           setHiddenColumns={setHiddenColumns}
