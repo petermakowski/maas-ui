38484b02103e3fa9c312b5194a38b65666ec1e19
Peter Makowski <me+git@petermakowski.io>
2024-03-26 14:49:32 +0100

    test: images change source

--- cypress/e2e/with-users/images/list.spec.ts
+++ cypress/e2e/with-users/images/list.spec.ts
@@ -9,4 +9,16 @@ context("Images list", () => {
   it("renders the correct heading", () => {
     cy.get("[data-testid='main-toolbar-heading']").contains("Images");
   });
+
+  it("changes source to custom and connects", () => {
+    cy.waitForPageToLoad();
+    cy.findByRole("button", { name: "Change source" }).click();
+    cy.findByRole("radio", { name: "Custom" }).click({ force: true });
+    cy.findByRole("textbox", { name: "URL" }).type(
+      "http://images.maas.io/ephemeral-v3/candidate"
+    );
+    cy.findByRole("button", { name: "Connect" }).click();
+    cy.waitForPageToLoad();
+    cy.findByText("Showing images fetched from").should("be.visible");
+  });
 });
